1d6d32161ba234294f11808b3d454d158f46ffb6
Nuatu <student@epicodus.com>
2014-08-23 16:44:41 -0700

    list events for the current day

--- lib/event.rb
+++ lib/event.rb
@@ -4,4 +4,8 @@ class Event < ActiveRecord::Base
     Event.where(:start => Time.now..'2400/01/01 12:00:00').sort_by_date
   end
 
+  def self.view_day(attributes = {:year => Time.now.year, :month => Time.now.month, :day => Time.now.day})
+    today = Date.new(attributes[:year], attributes[:month], attributes[:day])
+    Event.where(:start => "#{today.convert_to_sql}").sort_by_date
+  end
 end
--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -6,11 +6,20 @@ describe Event do
   it { should have_db_column :end }
 
   describe 'future' do
-    it 'should list all future events by date' do
+    it 'should list future events by date' do
       test_event = Event.create(:description => 'meeting', :location => 'work', :start => '2014/08/05', :end => '2014/08/06 12:00:00')
       test_event2 = Event.create(:description => 'son-birthday', :location => 'chuck-e-cheese', :start => '2015/02/02', :end => '2015/02/03 12:00:00')
       test_event3 = Event.create(:description => 'wedding', :location => '5th-st-reception-hall', :start => '2014/09/03', :end => '2014/09/04 12:00:00')
       expect(Event.future).to eq [test_event3, test_event2]
+    end
+  end
+
+   describe 'view_day' do
+    it 'should list events for the current day' do
+      test_event = Event.create(:description => 'meeting', :location => 'work', :start => '2014/08/05', :end => '2014/08/06 12:00:00')
+      test_event2 = Event.create(:description => 'son-birthday', :location => 'chuck-e-cheese', :start => '2014/08/23', :end => '2014/08/23 12:00:00')
+      test_event3 = Event.create(:description => 'wedding', :location => '5th-st-reception-hall', :start => '2014/09/03', :end => '2014/09/04 12:00:00')
+      expect(Event.view_day).to eq [test_event2]
     end
   end
 

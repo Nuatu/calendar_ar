cfa8a826503c5900eb7f30fb4a387887bf1a37f3
Nuatu <student@epicodus.com>
2014-08-24 23:05:50 -0700

    add test coverage for note class

--- spec/note_spec.rb
+++ spec/note_spec.rb
@@ -2,4 +2,15 @@ require 'spec_helper'
 describe Note do
   it { should have_db_column :description }
   it { should validate_presence_of :description }
+
+  describe '.all' do
+    it 'should list all notes' do
+      note1 = Note.create(:description => 'meeting')
+      note2 = Note.create(:description => 'son-birthday')
+      note3 = Note.create(:description => 'wedding')
+      note4 = Note.create(:description => 'daughter_birthday')
+      note5 = Note.create(:description => 'wife_birthday')
+      expect(Note.all).to eq [note1, note2, note3, note4, note5]
+    end
+  end
 end

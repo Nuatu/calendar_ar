30141e309aa2b8cfede15e731b26cfa4ec770af9
Nuatu <student@epicodus.com>
2014-08-24 21:22:32 -0700

    refactor to_do spec

--- spec/to_do_spec.rb
+++ spec/to_do_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 describe To_do do
-  it { should have_db_column :description}
+  it { should have_db_column :description }
 
   describe '.all' do
     it 'should list all to_dos' do

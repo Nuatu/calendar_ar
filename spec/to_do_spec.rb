8e3650ed66b450b485461c3a4de0dee22d3158d7
Nuatu <student@epicodus.com>
2014-08-24 20:26:45 -0700

    add spec coverage for to_do class

---
+++ spec/to_do_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+describe To_do do
+  it { should have_db_column :description}
+
+  describe '.all' do
+    it 'should list all to_dos' do
+      test_to_do1 = To_do.create(:description => 'meeting')
+      test_to_do2 = To_do.create(:description => 'son-birthday')
+      test_to_do3 = To_do.create(:description => 'wedding')
+      test_to_do4 = To_do.create(:description => 'daughter_birthday')
+      test_to_do5 = To_do.create(:description => 'wife_birthday')
+      expect(To_do.all).to eq [test_to_do1, test_to_do2, test_to_do3, test_to_do4, test_to_do5]
+    end
+  end
+end
